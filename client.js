2b8b9ddcc6e042839eaca62b32b0c248f26fe0eb
Filipe Moscovo <your@email.com>
2023-07-05 15:10:20 -0600

    adding moves

--- client.js
+++ client.js
@@ -21,7 +21,39 @@ const connect = function () {
     conn.write("Name: FIL");
   });
 
+  // event handling for input
+  const handleUserInput = function (key) {
+    switch (key) {
+      case "w":
+        conn.write("Move: up");
+        break;
+      case "s":
+        conn.write("Move: down");
+        break;
+      case "a":
+        conn.write("Move: left");
+        break;
+      case "d":
+        conn.write("Move: right");
+        break;
+      default:
+        break;
+    }
+  };
+
+  // input handling
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+
   return conn;
 };
 
 module.exports = { connect };
+
+// Move: up" - move up one square (unless facing down)
+// "Move: down" - move down one square (unless facing up)
+// "Move: left" - move left one square (unless facing right)
+// "Move: right" - move left one square (unless facing left)
